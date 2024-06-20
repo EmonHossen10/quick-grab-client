3b098cdad9189556e2e01af99be84ffba34f8415
unknown <emonhossen1000@gmail.com>
2024-06-20 12:42:14 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h2>content is commingggg......</h2>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Pages/Shared/Navbar.jsx
@@ -0,0 +1,114 @@
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  const navOptions = (
+    <>
+      <NavLink
+        to="/"
+        className={({ isActive, isPending }) =>
+          isPending
+            ? "pending"
+            : isActive
+            ? "text-basic underline   font-semibold "
+            : "hover:text-basic hover:underline "
+        }
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/about"
+        className={({ isActive, isPending }) =>
+          isPending
+            ? "pending"
+            : isActive
+            ? "text-basic underline font-semibold  "
+            : "hover:text-basic hover:underline "
+        }
+      >
+        About
+      </NavLink>
+
+      <NavLink
+        to="/experience"
+        className={({ isActive, isPending }) =>
+          isPending
+            ? "pending"
+            : isActive
+            ? "text-basic underline font-semibold  "
+            : "hover:text-basic hover:underline "
+        }
+      >
+        Experience
+      </NavLink>
+
+      <NavLink
+        to="/projects"
+        className={({ isActive, isPending }) =>
+          isPending
+            ? "pending"
+            : isActive
+            ? "text-basic underline font-semibold  "
+            : "hover:text-basic hover:underline "
+        }
+      >
+        Project
+      </NavLink>
+
+      <NavLink
+        to="/contact"
+        className={({ isActive, isPending }) =>
+          isPending
+            ? "pending"
+            : isActive
+            ? "text-basic underline font-semibold  "
+            : "hover:text-basic hover:underline "
+        }
+      >
+        Contact
+      </NavLink>
+    </>
+  );
+  return (
+    <>
+      <div className="navbar bg-base-100">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-md dropdown-content gap-3 mt-3 z-[1] p-2 shadow bg-[#142d33] text-white font-semibold rounded-box w-52  "
+            >
+              {navOptions}
+            </ul>
+          </div>
+          <a className="btn btn-ghost font-bold  text-2xl">Quick Grab</a>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal gap-8 text-[22px]     px-1">
+            {navOptions}
+          </ul>
+        </div>
+        <div className="navbar-end">
+          <a className="btn">Button</a>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
