1fee95fc7292417feed0da70b1e39097dfb252c1
unknown <emonhossen1000@gmail.com>
2024-06-21 12:26:36 +0600

    paralax added in featured section

--- src/Pages/Home/Featured/Featured.jsx
+++ src/Pages/Home/Featured/Featured.jsx
@@ -1,31 +1,34 @@
 import SectionTitle from "../../../Components/SectionTitle";
 import featuredImg from "../../../../public/asset/home/featured.jpg";
+import "./Featured.css";
 
 const Featured = () => {
   return (
-    <div>
-      <SectionTitle
-        subHeading={"check it out "}
-        heading={"featured items"}
-      ></SectionTitle>
-      <section className="flex justify-center items-center py-8 px-16 gap-10">
-        <div className="flex-1">
-          <img src={featuredImg} className="w-[600px]" alt="" />
-        </div>
-        <div className="flex-1">
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores,
-            ut.
-          </p>
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias
-            autem consequuntur neque quia dolore, porro ratione expedita iure
-            sint vero. 
-          </p>
-          <button className="btn btn-outline  btn-warning">CLICK HERE</button>
-        </div>
-      </section>
+    <div className="featured-item bg-fixed text-white pt-8 my-10">
+      <div>
+        <SectionTitle  
+          subHeading={"check it out "}
+          heading={"featured items"}
+        ></SectionTitle>
+        <section className="flex justify-center items-center bg-slate-500 bg-opacity-60 py-20 px-36 gap-10">
+          <div className="flex-1">
+            <img src={featuredImg} className="w-[600px]" alt="" />
+          </div>
+          <div className="flex-1">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores,
+              ut.
+            </p>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Molestias autem consequuntur neque quia dolore, porro ratione
+              expedita iure sint vero.
+            </p>
+            <button className="btn btn-outline  border-0 border-b-4    ">CLICK HERE</button>
+          </div>
+        </section>
+      </div>
     </div>
   );
 };
