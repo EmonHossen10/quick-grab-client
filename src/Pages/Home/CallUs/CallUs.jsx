3c4ad683da88e6cb564aab42d0aa51a2da50bd13
unknown <emonhossen1000@gmail.com>
2024-06-21 16:02:00 +0600

    call us added

---
+++ src/Pages/Home/CallUs/CallUs.jsx
@@ -0,0 +1,11 @@
+const CallUs = () => {
+  return (
+    <div  >
+      <div className="bg-black p-10 lg:p-20 mt-10 rounded-xl  w-11/12 mx-auto">
+        <h2 className="text-white text-center  uppercase lg:text-3xl font-bold" >Call Us: +88 0192345678910</h2>
+      </div>
+    </div>
+  );
+};
+
+export default CallUs;
--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,4 +1,5 @@
 import Banner from "../Banner/Banner";
+import CallUs from "../CallUs/CallUs";
 import ChefRecommendation from "../ChefRecomendation/ChefRecommendation";
 import Featured from "../Featured/Featured";
 import Testimonials from "../Testimonials/Testimonials";
@@ -12,6 +13,7 @@ const Home = () => {
       <div className="w-11/12   mx-auto py-4">
         <Category></Category>
         <PopularMenu></PopularMenu>
+        <CallUs></CallUs>
         <ChefRecommendation></ChefRecommendation>
         <Featured></Featured>
         <Testimonials></Testimonials>
