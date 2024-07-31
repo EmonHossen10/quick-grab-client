307d7a3f300644aef13f24fc77df07d4cfae82f2
unknown <emonhossen1000@gmail.com>
2024-07-31 15:31:58 +0600

    password toggle done

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,8 +1,27 @@
+import { useEffect, useState } from "react";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 import login from "../../../public/asset/login.jpg";
 import loginBG from "../../../public/asset/loginBG.jpg";
 import LogButton from "../../Components/LogButton";
+import {
+  loadCaptchaEnginge,
+  LoadCanvasTemplate,
+  LoadCanvasTemplateNoReload,
+  validateCaptcha,
+} from "react-simple-captcha";
 
 const Login = () => {
+  const [passwordVisible, setPasswordVisible] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setPasswordVisible(!passwordVisible);
+  };
+
+  // captcha
+  useEffect(() => {
+    loadCaptchaEnginge(6);
+  }, []);
+
   const handleLogin = (e) => {
     e.preventDefault();
     const form = e.target;
@@ -24,7 +43,9 @@ const Login = () => {
           <img src={login} alt="" />
         </div>
         <div className="card bg-base-100 first: lg:w-1/2 shadow-2xl">
-          <h1 className="text-4xl px-5 pt-5 font-bold">Please Login !!</h1>
+          <div className="flex justify-center border-b-4 w-6/12 items-center mx-auto pb-3 border-basic rounded-lg">
+            <h1 className="text-4xl  px-5 pt-5 font-bold">Login</h1>
+          </div>
           <form onSubmit={handleLogin} className="card-body">
             <div className="form-control">
               <label className="label">
@@ -34,27 +55,50 @@ const Login = () => {
                 type="email"
                 name="email"
                 placeholder="email"
-                className="input input-bordered"
+                className="input input-bordered  focus:border-basic focus:border-[1px]  focus:ring-basic focus:ring-[1px] focus:outline-none"
                 required
               />
             </div>
+
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Password</span>
               </label>
+              <div className="relative">
+                <input
+                  type={passwordVisible ? "text" : "password"}
+                  name="password"
+                  placeholder="password"
+                  className="input input-bordered w-full pr-10 focus:border-basic focus:border-[1px]  focus:ring-basic focus:ring-[1px] focus:outline-none  "
+                  required
+                />
+                <button
+                  onClick={togglePasswordVisibility}
+                  className="absolute inset-y-0 right-0 pr-3 flex items-center text-gray-500"
+                  type="button" // To prevent form submission on click
+                >
+                  {passwordVisible ? (
+                    <FaEyeSlash size={20} />
+                  ) : (
+                    <FaEye size={20} />
+                  )}
+                </button>
+              </div>
+            </div>
+            {/* captcha */}
+            <div className="form-control">
+              <label className="label">
+                <LoadCanvasTemplate />
+              </label>
               <input
-                type="password"
-                name="password"
-                placeholder="password"
-                className="input input-bordered"
+                type="text"
+                name="captcha"
+                placeholder="Type the text above"
+                className="input input-bordered focus:border-basic focus:border-[1px]  focus:ring-basic focus:ring-[1px] focus:outline-none"
                 required
               />
-              <label className="label">
-                <a href="#" className="label-text-alt link link-hover">
-                  Forgot password?
-                </a>
-              </label>
             </div>
+
             <div className="form-control mt-6">
               <LogButton text="Login"></LogButton>
             </div>
