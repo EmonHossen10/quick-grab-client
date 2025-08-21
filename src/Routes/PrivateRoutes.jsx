d8bc9efe70282ea5c1416b14df95320ea16013ce
EmonHossen10 <emonhossen1000@gmail.com>
2025-08-21 23:26:50 +0600

    loading added

--- src/Routes/PrivateRoutes.jsx
+++ src/Routes/PrivateRoutes.jsx
@@ -10,7 +10,8 @@ const PrivateRoutes = ({ children }) => {
     return (
       <>
         <div className="flex items-center justify-center h-screen bg-gray-100">
-          <span className="loading loading-bars loading-lg"></span>
+          {/* <span className="loading loading-bars loading-lg"></span> */}
+           <img src="https://i.pinimg.com/originals/6b/27/d1/6b27d169da9da73c3289a2088150c204.gif" alt="" />
         </div>
       </>
     );
